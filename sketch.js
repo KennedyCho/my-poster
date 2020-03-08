f498f0b67d344f526b001a5eecb1388d8303b2b2
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-08 00:22:03 -1000

    add twin suns

--- sketch.js
+++ sketch.js
@@ -1,18 +1,29 @@
-let w = 794; 
-let h = 1092; 
+let w = 794;
+let h = 1092;
 
-let weight = 0.7; 
+let weight = 0.8;
 
 function setup(){
 
-  createCanvas(w*weight, h*weight); 
-  background(200); 
+  createCanvas(w*weight, h*weight);
+  background(200);
   foreground();
+
+  sun();
+
+  // horizon();
+}
+
+function sun(){
+  noStroke();
+  fill("red");
+  ellipse(width/3, height/4, width/5);
+
+  ellipse(width*0.7, height*0.5, width/5);
 
 }
 
 function draw(){
-
 
 }
 
@@ -20,7 +31,7 @@ function foreground() {
   // stroke('red');
   // strokeWeight(5);
   noStroke();
-  fill(0); 
+  fill(0);
 
   // ground
   rect(0, height, width, -height/30);
@@ -33,4 +44,3 @@ function foreground() {
 
 
 }
-
